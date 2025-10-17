6f8e72d9d806a777ed884bf267533df4cb861651
RaZeRXNight <lazerx38@gmail.com>
2025-10-16 22:26:50 -0400

    Added Player, Asteroid, Asteroidfield Functionality

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame;
+from circleshape import CircleShape;
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    # Draws the asteroid in the shape of a circle, at the inputted position.
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        
+    # Moves the asteroid foward.
+    def update(self, dt):
+        self.position+= self.velocity * dt
+        
+    
--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 import pygame;
 from constants import *;
+from player import Player;
+from asteroid import Asteroid;
+from asteroidfield import AsteroidField;
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets the pygame's screen width and height.
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)); # Sets the pygame's screen width and height.
+    GAME_CLOCK = pygame.time.Clock();
+    
+    # Grouping Measures to make Drawing and Updating Easier, and lessens clutter in the code.
+    Updatable = pygame.sprite.Group()
+    Drawable = pygame.sprite.Group()
+    Asteroids = pygame.sprite.Group()
+    
+    Player.containers = (Updatable, Drawable); # A class variable which adds the Player Class to the specified Containers/Groups.
+    Asteroid.containers = (Asteroids, Updatable, Drawable); # Adds Asteroid Class to the specified Containers/Groups.
+    AsteroidField.containers = (Updatable);
+    
+    Field = AsteroidField();
+    PLAYER_1 = Player(SCREEN_WIDTH/2 + 50, SCREEN_HEIGHT/2);
+    
+    dt = 0;
     
     Game_State = True # Control Variable
     
+    # Main Loop for the Game.
     while Game_State:
+        # Checks for the quit game event, and if triggered, will quit the game and close the program.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return pygame.quit()
         
-        screen.fill("black") # Fills the background with the color black.
+        SCREEN.fill("black") # Fills the background with the color black.
+        
+        # Loops through every Updatable entity and updates them
+        for i in Updatable:
+            i.update(dt)
+        
+        # Loops through every drawable entity and draws them
+        for i in Drawable:
+            i.draw(SCREEN)
+            if i.Collision_Check(PLAYER_1) == True:
+                print("Game Over!")
+                pygame.quit()
+            
+        
         pygame.display.flip() # Refreshes the Screen
+        
+        dt = GAME_CLOCK.tick(60) / 1000 # Freezes the game for 1/60th of a second and assigns the delta time (time passed since last loop).
+        
     
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
